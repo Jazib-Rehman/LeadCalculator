57bb23a96d09f0469c38cfbaaf8f9c06846e5a44
Jazib Rehman <jazibrehman30@gmail.com>
2023-12-28 11:56:33 +0500

    cahnges

---
+++ src/Pages/ResultsCalculator.js
@@ -0,0 +1,12 @@
+import { FaCalculator } from "react-icons/fa";
+import './ResultsCalculator.css';
+
+const ResultsCalculator = () => {
+    return <div className="outer-container">
+        <div className="flex items-center">
+            <FaCalculator color="white" size={24} />
+        </div>
+    </div>
+}
+
+export default ResultsCalculator;
